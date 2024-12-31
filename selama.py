980dd064b8f68abb02ea17adcd4e063bd5c0f256
Asry31 <putraarsy86@gmail.com>
2025-01-01 05:26:38 +0700

    Add files via upload

---
+++ selama.py
@@ -0,0 +1,15 @@
+def ucapan_tahun_baru(nama_pacar):
+    ucapan = f"""
+    Happy New Year, {nama_pacar}!
+    Semoga tahun ini membawa lebih banyak kebahagiaan, cinta, dan kesuksesan untuk kita berdua. 
+    Aku sangat bersyukur bisa menjalani setiap hari bersamamu, dan aku berharap kita bisa terus tumbuh bersama di tahun yang baru ini.
+    Terima kasih sudah menjadi bagian dari hidupku. 
+    Mari hadapi tahun baru dengan penuh semangat dan cinta!
+    
+    Selamat Tahun Baru, sayang! ❤️
+    """
+    return ucapan
+
+# Ganti 'Nama Pacar' dengan nama pacarmu
+nama_pacar = "Rika Qaira Aryandra"
+print(ucapan_tahun_baru(nama_pacar))
